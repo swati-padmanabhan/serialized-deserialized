b0efe3080ed77638192638ccfd53b827164720d0
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-22 21:11:12 +0530

    initial commit of all files

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeserializeOnLists.models
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Person(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+            
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Text.Json;
+using SerializeDeserializeOnLists.models;
+
+namespace SerializeDeserializeOnLists
+{
+    internal class Program
+    {
+        static string path = ConfigurationManager.AppSettings["filePath"]!.ToString();
+
+        static void Main(string[] args)
+        {
+
+            if (File.Exists(path))
+            {
+                //deserialize the list of persons from the file
+                DeserializeObject();
+            }
+            else
+            {
+                //create a list of person objects
+                List<Person> persons = new List<Person>
+            {
+                new Person(101, "John", "john@gmail.com"),
+                new Person(102, "Jack", "jack@gmail.com"),
+                new Person(103, "Bob", "bob@gmail.com"),
+                new Person(104, "Jill", "jill@gmail.com"),
+                new Person(105, "Mack", "mach@gmail.com")
+            };
+
+                //serialize the list of persons to the file
+                SerializeObjects(persons);
+            }
+
+        }
+        static void SerializeObjects(List<Person> persons)
+        {
+            //here we open the file for writing
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+
+                //serialize the list of persons and write it to the file
+                sw.WriteLine(JsonSerializer.Serialize(persons));
+                Console.WriteLine("==========Serialized Objects==========");
+                Console.WriteLine("Persons Objects Written To File");
+            }
+        }
+        static void DeserializeObject()
+        {
+            //open the file for reading
+            using (StreamReader sr = new StreamReader(path))
+            {
+
+                //read the json file and deserialize it to a list of persons
+                List<Person> persons = JsonSerializer.Deserialize<List<Person>>(sr.ReadToEnd());
+
+                if (persons != null)
+                {
+                    Console.WriteLine("==========Deserialized Objects==========");
+                    foreach (var person in persons)
+                    {
+                        Console.WriteLine($"Id: {person.Id}\n" +
+                            $"Name: {person.Name}\n" +
+                            $"Email: {person.Email}\n");
+                    }
+                    Console.WriteLine($"Total Persons are: {persons.Count}");
+                }
+                else
+                {
+                    Console.WriteLine("No data found!");
+                }
+            }
+
+        }
+    }
+}
